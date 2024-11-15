9d15cfdf3e96d89e6640a7e4315c53294f80d81f
AndreyDolzhenko <adolsheet@gmail.com>
2024-11-15 17:48:35 +0300

    15112024

--- src/modules/dialoguesOfOperators/dialogues.controllers.js
+++ src/modules/dialoguesOfOperators/dialogues.controllers.js
@@ -1,7 +1,25 @@
+import moment from "moment-timezone";
+
 import Dialogue from "./models/dialoguesOfOperators.js";
 
-const getAllDialogues = async () => {
-    return Dialogue.findAll();
+const getAllDialogues = async () => { 
+
+  // return Dialogue.findAll();
+
+    const dialoguesRecords = await Dialogue.findAll();
+    const formattedRecords = dialoguesRecords.map((record) => {      
+      const formattedRecord = record;
+      formattedRecord.createdAt = moment(formattedRecord.createdAt)
+      .tz("UTC")
+      .utcOffset(180)
+      .format("YYYY-MM-DD HH:mm:ss");
+      formattedRecord.updatedAt = moment(formattedRecord.updatedAt).tz('UTC').utcOffset(180).format('YYYY-MM-DD HH:mm:ss');
+      console.log(formattedRecord);
+      return formattedRecord;
+    });  
+    
+    return formattedRecords;
+
   }
 
   const createDialogue = async (dialogue) => {
